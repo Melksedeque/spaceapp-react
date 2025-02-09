207cc15389f41baf64c39e82b1400d56d2ff0518
Melksedeque <freelancer@melksedeque.com.br>
2025-02-09 14:10:56 -0300

    Estilização e construção do SearchBox e SearchField

--- src/components/SearchField/index.jsx
+++ src/components/SearchField/index.jsx
@@ -1,48 +1,45 @@
 import styled from "styled-components";
+import search from "./search.png";
+
+const StyledSearchBox = styled.div`
+  background: linear-gradient(90deg, #c98cf1 0%, #7b78e5 100%);
+  border-radius: 0.675rem;
+  position: relative;
+  display: inline-block;
+  padding: 2px;
+`;
 
 const StyledSearchField = styled.input`
-  border: 2px solid transparent;
-  border-radius: 0.625rem;
+  background: linear-gradient(#041833 20%, #04244f 100%);
+  border: 0 none;
+  border-radius: 0.675rem;
+  box-sizing: border-box;
   color: #d9d9d9;
-  font-size: 1.25rem;
-  height: 40px;
+  font-weight: 400;
+  font-size: 20px;
+  height: 56px;
+  line-height: 20px;
   outline: none;
-  padding: 0 16px;
-  position: relative;
-  width: 100px;
-  &::before {
-    background: linear-gradient(to right, #c98cf1 0%, #7b78e5 100%);
-    content: "";
-    display: block;
-    height: 60px;
-    padding: 20px;
-    position: absolute;
-    right: 0;
-    top: 0;
-    transform: translate(-50%, -50%);
-    width: 200px;
-  }
-  &::after {
-    background: url("./search.png") no-repeat center right;
-    content: "";
-    display: block;
-    height: 2rem;
-    padding-right: 1rem;
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 2rem;
-    z-index: 10;
-  }
+  padding: 12px 16px;
+  width: 566px;
   &::placeholder {
     color: #d9d9d9;
   }
-  &:focus {
-    border-bottom-color: #7b78e5;
-    border-top-color: #7b78e5;
-  }
+`;
+
+const StyledSearchIcon = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 38px;
+  height: 38px;
 `;
 
 export default function SearchField() {
-  return <StyledSearchField type="text" placeholder="O que você procura?" />;
+  return (
+    <StyledSearchBox>
+      <StyledSearchField placeholder="O que você procura?" />
+      <StyledSearchIcon src={search} alt="Ícone de lupa para busca" />
+    </StyledSearchBox>
+  );
 }
