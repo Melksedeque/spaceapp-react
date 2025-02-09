c917d7055d4b897f47515345186a6dbbb9791001
Melksedeque <freelancer@melksedeque.com.br>
2025-02-09 13:14:15 -0300

    Criação de componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import SearchField from "../SearchField";
+
+const StyledHeader = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 3.75rem 0;
+  .logo {
+    display: flex;
+    align-items: center;
+    max-width: 212px;
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+    h1 {
+      /**
+        IMPORTAR FONTE PADRÃO
+        */
+      font-size: 1rem;
+    }
+  }
+  .searchBar {
+    display: flex;
+    align-items: center;
+    width: 600px;
+  }
+`;
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <div className="logo">
+        <img src="/imagens/logo.png" alt="logo" />
+        <h1 hidden>SpaceApp - React + Vite</h1>
+      </div>
+      <div className="searchBar">
+        <SearchField />
+      </div>
+    </StyledHeader>
+  );
+}
