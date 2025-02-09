a505ba4360abcc26a48d26e45029867383c3e94e
Melksedeque <freelancer@melksedeque.com.br>
2025-02-09 16:52:07 -0300

    Chamada do componente Banner e criação de HTML HardCoded para visualização de estruturas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./components/GlobalStyles";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Banner from "./components/Banner";
 
 const GradientBackground = styled.div`
   background: linear-gradient(
@@ -14,12 +15,79 @@ const GradientBackground = styled.div`
   width: 100%;
 `;
 
+const MainContainer = styled.div`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  main {
+    flex: 1;
+    padding-left: 2rem;
+  }
+`;
+
 function App() {
   return (
     <GradientBackground>
       <GlobalStyles />
       <Header />
-      <Sidebar />
+      <MainContainer>
+        <Sidebar />
+        <main>
+          <Banner>A galeria mais completa de fotos do espaço!</Banner>
+          <section className="tags-navigation">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">Estrelas</a>
+                </li>
+                <li>
+                  <a href="#">Galáxia</a>
+                </li>
+                <li>
+                  <a href="#">Lua</a>
+                </li>
+                <li>
+                  <a href="#">Planetas</a>
+                </li>
+                <li>
+                  <a href="#">Todas</a>
+                </li>
+              </ul>
+            </nav>
+          </section>
+          <section className="gallery-container">
+            <h2>Navegue pela galeria</h2>
+            <section className="gallery">
+              <figure>
+                <img src="imagem1.jpg" alt="Imagem 1" />
+              </figure>
+              <figure>
+                <img src="imagem2.jpg" alt="Imagem 2" />
+              </figure>
+              <figure>
+                <img src="imagem3.jpg" alt="Imagem 3" />
+              </figure>
+              <figure>
+                <img src="imagem4.jpg" alt="Imagem 4" />
+              </figure>
+            </section>
+
+            <aside className="popular-gallery">
+              <h2>Populares</h2>
+              <figure>
+                <img src="popular1.jpg" alt="Popular 1" />
+              </figure>
+              <figure>
+                <img src="popular2.jpg" alt="Popular 2" />
+              </figure>
+              <figure>
+                <img src="popular3.jpg" alt="Popular 3" />
+              </figure>
+            </aside>
+          </section>
+        </main>
+      </MainContainer>
     </GradientBackground>
   );
 }
