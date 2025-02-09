1f69952df6edf5c49fdf02dd013fcdac6cd3f112
Melksedeque <freelancer@melksedeque.com.br>
2025-02-09 16:51:30 -0300

    Criação de componente Banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import banner from "./banner.png";
+
+const StyledBanner = styled.section`
+  figure {
+    border-radius: 2rem;
+    display: block;
+    height: 330px;
+    margin: 0;
+    overflow: hidden;
+    padding: 0;
+    position: relative;
+    img {
+      height: 100%;
+      object-fit: cover;
+      width: 100%;
+    }
+    figcaption {
+      color: #d9d9d9;
+      font: normal 2.5rem GandhiSansRegular;
+      position: absolute;
+      left: 2rem;
+      margin: 0;
+      padding: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 300px;
+    }
+  }
+`;
+
+export default function MainBanner({ children }) {
+  return (
+    <StyledBanner>
+      <figure>
+        <img src={banner} alt="A imagem de um astro" />
+        <figcaption>{children}</figcaption>
+      </figure>
+    </StyledBanner>
+  );
+}
