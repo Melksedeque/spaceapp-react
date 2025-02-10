f33315eb2b606673ee8e8f92887a45566dccc419
Melksedeque <freelancer@melksedeque.com.br>
2025-02-10 15:11:20 -0300

    Tratamento de props e alinhamento de acordo com a prop

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -3,8 +3,9 @@ import styled from "styled-components";
 const StyledTitle = styled.h2`
   color: #7b78e5;
   font: normal 2rem GandhiSansRegular;
+  text-align: ${(props) => props.$align || "left"};
 `;
 
-export default function Title({ children }) {
-  return <StyledTitle>{children}</StyledTitle>;
+export default function Title({ children, align }) {
+  return <StyledTitle $align={align}>{children}</StyledTitle>;
 }
