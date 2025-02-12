6c2554a307b720ec411f25754211deea4b74e38d
Melksedeque <freelancer@melksedeque.com.br>
2025-02-12 18:39:27 -0300

    Passagem de key na prop do GalleryImage

--- src/components/Gallery/MainGallery/index.jsx
+++ src/components/Gallery/MainGallery/index.jsx
@@ -1,5 +1,6 @@
 import { styled } from "styled-components";
 import Title from "../../Title";
+import GalleryImage from "../GalleryImage";
 
 const StyledMainGallery = styled.section`
   display: flex;
@@ -9,32 +10,6 @@ const StyledMainGallery = styled.section`
   h2 {
     width: 100%;
   }
-  .main-gallery__image {
-    border-radius: 0.5rem;
-    cursor: pointer;
-    flex: 0 1 450px;
-    margin: 0;
-    overflow: hidden;
-    padding: 0;
-    position: relative;
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-
-    figcaption {
-      background: #001634;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      padding: 0.5rem;
-      color: #fff;
-      font-size: 0.8rem;
-      text-align: center;
-    }
-  }
 `;
 
 export default function MainGallery({ title, pictures = [] }) {
@@ -42,12 +17,7 @@ export default function MainGallery({ title, pictures = [] }) {
     <StyledMainGallery>
       <Title>{title}</Title>
       {pictures.map((picture) => (
-        <figure className="main-gallery__image" key={picture.id}>
-          <img src={picture.path} alt={`${picture.titulo}, ${picture.fonte}`} />
-          <figcaption>
-            {picture.titulo} in {picture.tagId}
-          </figcaption>
-        </figure>
+        <GalleryImage key={picture.id} image={picture} gallery="main" />
       ))}
     </StyledMainGallery>
   );
