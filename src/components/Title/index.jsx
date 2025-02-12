4f640e129279a6af3c5f845e64df27550e71a808
Melksedeque <freelancer@melksedeque.com.br>
2025-02-12 20:14:16 -0300

    Remoção de margin no h2

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledTitle = styled.h2`
   color: #7b78e5;
   font: normal 2rem GandhiSansRegular;
+  margin: 0;
   text-align: ${(props) => props.$align || "left"};
 `;
 
