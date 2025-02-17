d802e50665ecd8d4c2ee35c1c74d546b45f63f23
Melksedeque <freelancer@melksedeque.com.br>
2025-02-17 13:35:58 -0300

    Criação de Utils para tratar Tags e Imagens

---
+++ src/utils/galleryUtils.jsx
@@ -0,0 +1,25 @@
+import tags from "../components/Gallery/FilterByTags/tags.json";
+
+export const getTagById = (tagId) => {
+  const tag = tags.find((tag) => tag.id === tagId);
+  return tag ? tag.titulo : "Tag não encontrada";
+};
+
+export const filterImagesByTag = (images, selectedTagId) => {
+  if (selectedTagId === 0) return images;
+  return images.filter((image) => image.tagId === selectedTagId);
+};
+
+export const getTagsWithCount = (images) => {
+  return tags.map((tag) => ({
+    ...tag,
+    count:
+      tag.id === 0
+        ? images.length
+        : images.filter((image) => image.tagId === tag.id).length,
+  }));
+};
+
+export const imageHasTag = (image, tagId) => {
+  return tagId === 0 || image.tagId === tagId;
+};
