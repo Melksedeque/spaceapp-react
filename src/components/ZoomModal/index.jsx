4c65cc183724f9ee4f46882d2dbade63d4d0ce7b
Melksedeque <freelancer@melksedeque.com.br>
2025-02-16 21:22:54 -0300

    Criação de Overlay para exibir o dialog

--- src/components/ZoomModal/index.jsx
+++ src/components/ZoomModal/index.jsx
@@ -1,15 +1,34 @@
+import styled from "styled-components";
 import GalleryImage from "../Gallery/GalleryImage";
+
+const Overlay = styled.div`
+  align-items: center;
+  background: rgba(0, 0, 0, 0.7);
+  bottom: 0;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 100%;
+  dialog {
+  }
+`;
 
 export default function ZoomModal({ picture }) {
   return (
     <>
       {picture && (
-        <dialog open={!!picture}>
-          <GalleryImage image={picture} expanded={true} />
-          <form method="dialog">
-            <button>Ok!</button>
-          </form>
-        </dialog>
+        <Overlay>
+          <dialog open={!!picture}>
+            <GalleryImage image={picture} expanded={true} />
+            <form method="dialog">
+              <button>Ok!</button>
+            </form>
+          </dialog>
+        </Overlay>
       )}
     </>
   );
