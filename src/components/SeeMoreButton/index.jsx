b234cef4e14efbcad8a76509a4aedbf0915e0abb
Melksedeque <freelancer@melksedeque.com.br>
2025-02-17 19:57:23 -0300

    Criação de componente para Ver mais

---
+++ src/components/SeeMoreButton/index.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const StyledSeeMore = styled.div`
+  background: linear-gradient(90deg, #c98cf1 0%, #7b78e5 100%);
+  border-radius: 10px;
+  display: block;
+  padding: 2px;
+  width: 100%;
+  button {
+    background: #092d5c;
+    border: 0 none;
+    border-radius: 10px;
+    color: #ffffff;
+    cursor: pointer;
+    display: block;
+    font: 700 1.25rem GandhiSansBold;
+    padding: 1rem 2rem;
+    transition: all 0.25s ease-in-out;
+    width: 100%;
+    &:hover {
+      background-color: transparent;
+    }
+  }
+`;
+
+export default function SeeMoreButton() {
+  return (
+    <StyledSeeMore>
+      <button>Ver mais</button>
+    </StyledSeeMore>
+  );
+}
