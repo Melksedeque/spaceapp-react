84111827ed1686bfdbd6b9bd38330e6f229a9a09
Melksedeque <freelancer@melksedeque.com.br>
2025-02-16 21:02:15 -0300

    Criação de figure hard-coded no modal para teste de visualização

--- src/components/ZoomModal/index.jsx
+++ src/components/ZoomModal/index.jsx
@@ -1,10 +1,15 @@
 export default function ZoomModal({ picture }) {
+  console.log(picture);
   return (
     <>
       <dialog open={!!picture}>
         <p>
           Olá, eu sou um <strong>modal</strong>!
         </p>
+        <figure>
+          <img src={picture.path} alt={picture.titulo} />
+          <figcaption>{picture.titulo}</figcaption>
+        </figure>
         <form method="dialog">
           <button>Ok!</button>
         </form>
