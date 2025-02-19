3b85bd660df54aea25d8a9adf158d0b44c9b55f7
Melksedeque <freelancer@melksedeque.com.br>
2025-02-19 17:32:06 -0300

    Estilo de cores dos ícones sociais

--- src/components/SocialIcon/index.jsx
+++ src/components/SocialIcon/index.jsx
@@ -1,7 +1,22 @@
+import styled from "styled-components";
+
+const StyledSocialIcon = styled.a`
+  img {
+    display: block;
+    transition: all 0.25s ease-in-out;
+  }
+  &:hover {
+    img {
+      filter: invert(38%) sepia(76%) saturate(294%) hue-rotate(206deg)
+        brightness(91%) contrast(88%);
+    }
+  }
+`;
+
 export default function SocialIcon({ icon, name, link }) {
   return (
-    <a href={link} target="_blank" rel="noreferrer" className="social-icon">
+    <StyledSocialIcon href={link} target="_blank" rel="noreferrer">
       <img src={icon} alt={`Social icon ${name}`} />
-    </a>
+    </StyledSocialIcon>
   );
 }
