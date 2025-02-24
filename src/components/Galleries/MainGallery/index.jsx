27da2d5851f7e483080225f73cb932bde4d67f6e
Melksedeque <freelancer@melksedeque.com.br>
2025-02-24 12:33:42 -0300

    Criação de componente separado para Galeria Principal, dividindo a responsabilidade

---
+++ src/components/Galleries/MainGallery/index.jsx
@@ -0,0 +1,23 @@
+import { styled } from "styled-components";
+import Gallery from "../Gallery";
+import { filterImages } from "../../../utils/galleryUtils";
+
+const MainGalleryContainer = styled.div`
+  flex: 1;
+`;
+
+export default function MainGallery({ pictures, selectedTag, searchTerm, onSelectedPicture }) {
+  const filteredPictures = filterImages(pictures, selectedTag, searchTerm);
+
+  return (
+    <MainGalleryContainer>
+      <Gallery
+        alignment="left"
+        title="Navegue pela galeria"
+        pictures={filteredPictures}
+        onSelectedPicture={onSelectedPicture}
+        dataGallery="main"
+      />
+    </MainGalleryContainer>
+  );
+}
